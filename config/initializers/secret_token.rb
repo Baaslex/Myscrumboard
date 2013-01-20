981e58920cfe05a61d268f6adf8aa7f225e5a813
Nicky Heuperman <nicky@dev-ice.com>
2013-01-20 15:41:59 +0100

    barebones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myscrumboard::Application.config.secret_token = 'a7f73d874d23c0f319f3916b39a07c5730106ba2f3e80c410aa03f10c54eb5a531a9a4d9deefab2a4fac45691f858da099362b81f7bfb167720831c60b10260e'
